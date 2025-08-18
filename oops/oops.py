5653a18c64b6d499846d61b703bbab53d66e6fa3
vineet kushwaha <vineetkushwaha2010@gmail.com>
2025-08-18 21:51:36 +0530

    few changes

--- oops/oops.py
+++ oops/oops.py
@@ -18,17 +18,53 @@
 # obj1.stu_detail()
 
 # instance variable
-class Student:
-    def __init__(self,name,age,roll):
-        self.n=name
-        self.a=age
-        self.r=roll
-    def show(self):
-        print(self.n)
-        print(self.a)
-        print(self.r)
-obj1=Student('vineet',23,110)
-obj2=Student('rohit',21,120)
+# class Student:
+#     def __init__(self,name,age,roll):
+#         self.n=name
+#         self.a=age
+#         self.r=roll
+#     def show(self):
+#         print(self.n)
+#         print(self.a)
+#         print(self.r)
+# obj1=Student('vineet',23,110)
+# obj2=Student('rohit',21,120)
 
+# obj1.show()
+# obj2.show()
+
+#static method
+# class Student:
+#     def __init__(self,name,city):
+#         self.name=name
+#         self.city=city
+#     @staticmethod
+#     def add():
+#         print(self.name)
+#         print(self.city)
+# obj1=Student('vineet','bhopal')
+
+# obj1.add()
+
+
+
+#class method
+class Student:
+    school='shss'
+    def __init__(self,name,city):
+        self.name=name
+        self.city=city
+        
+    @classmethod
+    def update(cls,new):
+        print(id(cls))
+        cls.school=new
+    def show(self):
+        print(self.name, self.city, Student.school)
+
+print(id(Student))
+obj1=Student('vineet','bhopal')
+obj2=Student('neeraj','bhopal')
+Student.update('new_shss')
 obj1.show()
 obj2.show()
