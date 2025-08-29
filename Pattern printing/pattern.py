ab36abdc1865ae80b1886b846259c7a9ffc804b7
vineet kushwaha <vineetkushwaha2010@gmail.com>
2025-08-29 23:26:19 +0530

    new changes

--- Pattern printing/pattern.py
+++ Pattern printing/pattern.py
@@ -260,5 +260,42 @@
 # n=int(input("Enter any no:"))
 # for i in range(1,n+1):
 #     for j in range(1,n+1):
-#         print(j,end="")
+#         print(j,end=" ")
+#     print()
+
+
+
+# 1
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5
+# n=int(input("Enter any no:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5
+# n=int(input("Enter any no:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()
+
+
+# 1 1 1 1 1 
+# 2 2 2 2 2
+# 3 3 3 3 3
+# 4 4 4 4 4
+# 5 5 5 5 5
+# n=int(input("Enter any no:"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(i,end=" ")
 #     print()
