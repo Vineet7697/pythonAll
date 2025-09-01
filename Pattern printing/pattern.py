cb8e46588006c7f64a4ec7ec88effaca46e88fe7
vineet kushwaha <vineetkushwaha2010@gmail.com>
2025-09-01 21:49:29 +0530

    add new program

--- Pattern printing/pattern.py
+++ Pattern printing/pattern.py
@@ -299,3 +299,76 @@
 #     for j in range(1,n+1):
 #         print(i,end=" ")
 #     print()
+
+
+# A B C D E  
+# A B C D E 
+# A B C D E 
+# A B C D E 
+# A B C D E 
+# n=int(input("Enter any no:"))
+# for i in range(1,n+1):
+#     ch='A'
+#     for j in range(1,n+1):
+#         print(ch,end=" ")
+#         ch=chr(ord(ch)+1)
+#     print()
+
+
+
+
+# A  
+# A B 
+# A B C 
+# A B C D 
+# A B C D E 
+# n=int(input("Enter any no:"))
+# for i in range(1,n+1):
+#     ch='A'
+#     for j in range(1,i+1):
+#         print(ch,end=" ")
+#         ch=chr(ord(ch)+1)
+#     print()
+
+
+# 2  
+# 2 4 
+# 2 4 6 
+# 2 4 6 8 
+# 2 4 6 8 10 
+# n=int(input("Enter any no:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(2*j,end=" ")
+#     print()
+
+
+
+# 1  
+# 1 3 
+# 1 3 5 
+# 1 3 5 7 
+# 1 3 5 7 9 
+
+# n=int(input("Enter any no:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(2*j-1,end=" ")
+#     print()
+
+
+
+# 10
+# 10 8 
+# 10 8 6 
+# 10 8 6 4 
+# 10 8 6 4 2 
+
+# n=int(input("Enter any no:"))
+# k=[]
+# for i in range(10,1,-2):
+#     k.append(i)
+#     for j in k:
+#         print(j,end=" ")
+#     print()
+    
